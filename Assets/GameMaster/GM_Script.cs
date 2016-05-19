7e9e792b29b7bb0459a05640c573abd61a871d06
Ruban Ivan <ivan@ruban.pro>
2016-05-19 17:36:16 +0300

    add bkg

--- Assets/GameMaster/GM_Script.cs
+++ Assets/GameMaster/GM_Script.cs
@@ -10,13 +10,16 @@ public class GM_Script : MonoBehaviour
     // Use this for initialization
     void Start ()
 	{
-        //BrickList = new List<GameObject>();
         BrickList.Add(Resources.Load("Brick Blue") as GameObject);
         BrickList.Add(Resources.Load("Brick Green") as GameObject);
         BrickList.Add(Resources.Load("Brick Yellow") as GameObject);
 
+        for (int i = 0; i < 12; i++)
+        {
+            Instantiate(BrickList[0], new Vector3(-370+62*i, 250), Quaternion.identity);
+        }
 
-        Instantiate(BrickList[0], new Vector3(-370, 250),Quaternion.identity);
+        //Instantiate(BrickList[0], new Vector3(-370, 250),Quaternion.identity);
 
 	}
 	
