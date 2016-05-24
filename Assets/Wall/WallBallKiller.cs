0680c7c6763e5df77c28bc47c735834b874b32f3
Ruban Ivan <ivan@ruban.pro>
2016-05-24 10:18:03 +0300

    pered peredelkoi

---
+++ Assets/Wall/WallBallKiller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class WallBallKiller : MonoBehaviour
+{
+    public event Action BallKilled;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "BallTag")
+        {
+            //Destroy(collision.gameObject);
+            if (BallKilled != null) BallKilled();
+        }
+    }
+
+
+}
